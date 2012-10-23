e2eefc1fd261d6f9de7440a5f02b5a091d869632
Natty <nattanicha@gmail.com>
2012-10-23 14:59:08 +0700

    Pronto quiz on iOS

--- Quiz/QuizViewController.h
+++ Quiz/QuizViewController.h
@@ -10,4 +10,20 @@
 
 @interface QuizViewController : UIViewController
 
+{
+    int currentQuestionIndex;
+    
+    //The model objects
+    NSMutableArray *questions;
+    NSMutableArray *answers;
+    
+    //The view objects 
+    IBOutlet UILabel *questionField;
+    IBOutlet UILabel *answerField;
+}
+
+//The view objects
+- (IBAction)showQuestion:(id)sender;
+- (IBAction)showAnswer:(id)sender;
+
 @end
--- Quiz/QuizViewController.m
+++ Quiz/QuizViewController.m
@@ -14,21 +14,56 @@
 
 @implementation QuizViewController
 
-- (void)viewDidLoad
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    //call the init method implemented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if(self){
+        //create two arrays and make the pointers point to them
+        questions = [[NSMutableArray alloc] init];
+        answers = [[NSMutableArray alloc] init];
+        
+        //Add questions and answers to the arrays
+        [questions addObject:@"Who is Cory?"];
+        [answers addObject:@"Louis"];
+        
+        [questions addObject:@"What is the board we have in dancing room?"];
+        [answers addObject:@"Visual Kanban Board"];
+        
+        [questions addObject:@"What is Pronto Marketing?"];
+        [answers addObject:@"Internet Presence Management"];
+    }
+    //Return the address of the new object
+    return self;
 }
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
+-(IBAction)showQuestion:(id)sender{
+    //step to the next question
+    currentQuestionIndex++;
+    
+    //Am I past the last question?
+    if(currentQuestionIndex == [questions count]){
+        //Go back to the first question
+        currentQuestionIndex = 0;
+    }
+    //Get the string at that index in the questions array
+    NSString *question = [questions objectAtIndex:currentQuestionIndex];
+    
+    //Log the string to the console
+    NSLog(@"displaying question: %@", question);
+    
+    //Display the string in the question field
+    [questionField setText:question];
+    
+    //clear the answer field
+    [answerField setText:@"???"];
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+-(IBAction)showAnswer:(id)sender{
+    //What is the answer to the current question?
+    NSString *answer = [answers objectAtIndex:currentQuestionIndex];
+    
+    //Display it in the answer field
+    [answerField setText:answer];
 }
-
 @end
